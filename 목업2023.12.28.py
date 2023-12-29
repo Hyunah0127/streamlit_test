ae9d436653d22b5be1b95ba00328c84097d546e4
Hyunah <m01087112627@gmail.com>
2023-12-29 16:04:54 +0900

    video add

--- 목업2023.12.28.py
+++ 목업2023.12.28.py
@@ -49,21 +49,22 @@ if selected == emoji.emojize("실시간 CCTV"):
 
     with tab1:
                     
+         # 페이지 번호를 저장할 Session State 생성
+        if 'page' not in st.session_state:
+            st.session_state.page = 1
+        
         # 각 CCTV 위치에 해당하는 동영상 파일 경로 또는 URL
         video_file_paths = [
-            'https://raw.githubusercontent.com/Hyunah0127/streamlit_test/main/dataset/fi001.mp4',  # 첫 번째 CCTV 동영상 파일 경로
-            'https://raw.githubusercontent.com/Hyunah0127/streamlit_test/main/dataset/fi001.mp4',  # 두 번째 CCTV 동영상 파일 경로
-            'https://raw.githubusercontent.com/Hyunah0127/streamlit_test/main/dataset/fi001.mp4',
-            'https://raw.githubusercontent.com/Hyunah0127/streamlit_test/main/dataset/fi001.mp4',
-            'https://raw.githubusercontent.com/Hyunah0127/streamlit_test/main/dataset/fi001.mp4',
-            'https://raw.githubusercontent.com/Hyunah0127/streamlit_test/main/dataset/fi001.mp4',
-            'https://raw.githubusercontent.com/Hyunah0127/streamlit_test/main/dataset/fi001.mp4',
-            'https://raw.githubusercontent.com/Hyunah0127/streamlit_test/main/dataset/fi001.mp4',
-            'https://raw.githubusercontent.com/Hyunah0127/streamlit_test/main/dataset/fi001.mp4',
+            os.path.abspath('video/normal1.mp4'),  # CCTV 동영상 파일 경로
+            os.path.abspath('video/normal2.mp4'),
+            os.path.abspath('video/normal3.mp4'),
+            os.path.abspath('video/normal4.mp4'),
+            os.path.abspath('video/normal5.mp4'),
+            os.path.abspath('video/Faint-Detection.mp4')
         ]
-
+        
         # 3행 3열 그리드 생성 및 각 그리드에 동영상 삽입
-        for i in range(3):  # 3행
+        for i in range(2):  # 3행
             cols = st.columns(3)  # 각 행에 3열
             for j, col in enumerate(cols, start=1):
                 with col:
@@ -74,15 +75,86 @@ if selected == emoji.emojize("실시간 CCTV"):
                     st.video(video_file_paths[cctv_number - 1],start_time=0)
 
     with tab2:
-        st.write("2층")
+        # 페이지 번호를 저장할 Session State 생성
+        if 'page' not in st.session_state:
+            st.session_state.page = 1
+        
+        # 각 CCTV 위치에 해당하는 동영상 파일 경로 또는 URL
+        video_file_paths = [
+            os.path.abspath('video/normal5.mp4'),  # CCTV 동영상 파일 경로
+            os.path.abspath('video/normal6.mp4'),
+            os.path.abspath('video/normal7.mp4'),
+            os.path.abspath('video/normal8.mp4'),
+            os.path.abspath('video/normal9.mp4'),
+            os.path.abspath('video/normal10.mp4')
+        ]
+        
+        # 3행 3열 그리드 생성 및 각 그리드에 동영상 삽입
+        for i in range(2):  # 3행
+            cols = st.columns(3)  # 각 행에 3열
+            for j, col in enumerate(cols, start=1):
+                with col:
+                    # 현재 행과 열에 따라 CCTV 번호 계산
+                    cctv_number = i * 3 + j
+                    st.subheader(f"CCTV {cctv_number}")
+                    # 각 CCTV 번호에 해당하는 동영상 재생
+                    st.video(video_file_paths[cctv_number - 1],start_time=0)
 
     with tab3:
-        st.write("3층")
+        # 페이지 번호를 저장할 Session State 생성
+        if 'page' not in st.session_state:
+            st.session_state.page = 1
         
+        # 각 CCTV 위치에 해당하는 동영상 파일 경로 또는 URL
+        video_file_paths = [
+            os.path.abspath('video/normal8.mp4'),  # CCTV 동영상 파일 경로
+            os.path.abspath('video/normal7.mp4'),
+            os.path.abspath('video/normal6.mp4'),
+            os.path.abspath('video/normal5.mp4'),
+            os.path.abspath('video/normal4.mp4'),
+            os.path.abspath('video/normal3.mp4')
+        ]
+        
+        # 3행 3열 그리드 생성 및 각 그리드에 동영상 삽입
+        for i in range(2):  # 3행
+            cols = st.columns(3)  # 각 행에 3열
+            for j, col in enumerate(cols, start=1):
+                with col:
+                    # 현재 행과 열에 따라 CCTV 번호 계산
+                    cctv_number = i * 3 + j
+                    st.subheader(f"CCTV {cctv_number}")
+                    # 각 CCTV 번호에 해당하는 동영상 재생
+                    st.video(video_file_paths[cctv_number - 1],start_time=0)
+    
+    with tab4:
+        # 페이지 번호를 저장할 Session State 생성
+        if 'page' not in st.session_state:
+            st.session_state.page = 1
+        
+        # 각 CCTV 위치에 해당하는 동영상 파일 경로 또는 URL
+        video_file_paths = [
+            os.path.abspath('video/normal7.mp4'),  # CCTV 동영상 파일 경로
+            os.path.abspath('video/normal8.mp4'),
+            os.path.abspath('video/normal9.mp4'),
+            os.path.abspath('video/normal10.mp4'),
+            os.path.abspath('video/normal1.mp4'),
+            os.path.abspath('video/normal2.mp4')
+        ]
+        
+        # 3행 3열 그리드 생성 및 각 그리드에 동영상 삽입
+        for i in range(2):  # 3행
+            cols = st.columns(3)  # 각 행에 3열
+            for j, col in enumerate(cols, start=1):
+                with col:
+                    # 현재 행과 열에 따라 CCTV 번호 계산
+                    cctv_number = i * 3 + j
+                    st.subheader(f"CCTV {cctv_number}")
+                    # 각 CCTV 번호에 해당하는 동영상 재생
+                    st.video(video_file_paths[cctv_number - 1],start_time=0)
         
 
 # EVENT 누를 떄
-if selected == emoji.emojize("EVENT"):
+if selected == emoji.emojize("이상행동 발생 구역 모니터링"):
      # Markdown을 사용한 스타일링 타이틀
     st.markdown("""
         <style>
@@ -90,10 +162,24 @@ if selected == emoji.emojize("EVENT"):
             font-size:36px !important;
             font-weight: bold;
             color: #1B365C;
+            margin-top: -70px;  # 텍스트 위의 여백 줄임
         }
         </style>
         """, unsafe_allow_html=True)
 
-    st.markdown('<p class="big-font">사고발생 현황</p>', unsafe_allow_html=True)
+    st.markdown('<p class="big-font">이상행동 탐지 구역 세부 모니터링</p>', unsafe_allow_html=True)
+    
+    st.error('"실신" 행동 감지되었습니다.')
+        
+    cols = st.columns((8, 2))  # 페이지를 세로로 8:2 비율로 분할
+
+    with cols[0]:  # 좌측 페이지
+        # CCTV 영상 출력
+        st.video("video/falldown_detection.mp4", start_time=0)
+
+    with cols[1]:  # 우측 페이지
+        # 텍스트 출력
+        
+        st.write("사건 관련 인원: 2명")
 
    
